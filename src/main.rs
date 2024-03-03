351d9dac7bf5b2db1764e4166588fdf65044ac09
Nasserallah EL MASSAOUDI <nasserallah.elmassaoudi98@gmail.com>
2024-03-03 16:32:08 +0100

    add passive cleanup of values on expiry

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,11 @@ use std::{
     net::TcpListener,
     sync::{Arc, Mutex},
     thread,
+    time::Duration,
 };
 use types::Memory;
+
+use crate::helpers::get_current_timestamp;
 
 fn main() {
     let memory: Memory = Arc::new(Mutex::new(HashMap::new()));
@@ -28,6 +31,25 @@ fn main() {
     }
     let listner =
         TcpListener::bind(format!("127.0.0.1:{}", port)).expect("Failed to bind to port 6379");
+    let check_memory = Arc::clone(&memory);
+    let _j = thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(60));
+        let mut local_memory = check_memory.lock().unwrap();
+        let x = local_memory.clone();
+        println!("{:?}", x);
+        for (key, val) in x.iter() {
+            match val.expiry {
+                Some(ex) => {
+                    let now = get_current_timestamp();
+                    let created_at = val.created_at;
+                    if now - created_at >= ex {
+                        local_memory.remove(key);
+                    }
+                }
+                None => {}
+            }
+        }
+    });
     for stream in listner.incoming() {
         match stream {
             Ok(mut s) => {
