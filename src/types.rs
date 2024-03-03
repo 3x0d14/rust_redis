12ee291384285002da4fc45cb4e9679b448c6348
Nasserallah EL MASSAOUDI <nasserallah.elmassaoudi98@gmail.com>
2024-03-03 20:48:08 +0100

    config parsing

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,8 @@
-use crate::data::Val;
+use crate::data::{Config, Val};
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
 
 pub type Memory = Arc<Mutex<HashMap<String, Val>>>;
+pub type AConf = Arc<Mutex<Config>>;
