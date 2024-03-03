a856708110578e149b066a807834cfe92c5a73db
Nasserallah EL MASSAOUDI <nasserallah.elmassaoudi98@gmail.com>
2024-03-03 16:13:19 +0100

    add port arg

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod types;
 use handler::handle;
 use std::{
     collections::HashMap,
+    env,
     net::TcpListener,
     sync::{Arc, Mutex},
     thread,
@@ -16,7 +17,17 @@ use types::Memory;
 
 fn main() {
     let memory: Memory = Arc::new(Mutex::new(HashMap::new()));
-    let listner = TcpListener::bind("127.0.0.1:6379").expect("Failed to bind to port 6379");
+    let args: Vec<String> = env::args().collect();
+    let mut port = 6379;
+    if args.len() > 1 {
+        let arg = &args[1];
+        let val = &args[2];
+        if arg == "--port" {
+            port = val.parse::<i32>().unwrap();
+        }
+    }
+    let listner =
+        TcpListener::bind(format!("127.0.0.1:{}", port)).expect("Failed to bind to port 6379");
     for stream in listner.incoming() {
         match stream {
             Ok(mut s) => {
